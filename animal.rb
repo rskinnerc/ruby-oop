ebf6bb08efd3e15b4ecb86cf3e25107afd524e4f
Ronald <rjskinnerc@unal.edu.co>
2022-07-18 16:49:11 -0500

    Add foods class

--- animal.rb
+++ animal.rb
@@ -1,4 +1,5 @@
 require './remover'
+require "./foods.rb"
 
 class Animal
   def initialize(type, number_of_legs, name = 'Unknown')
@@ -6,17 +7,22 @@ class Animal
     @name = name
     @number_of_legs = number_of_legs
     @type = type
+    @liked_food = NoFood.new()
   end
 
   attr_accessor :name
   attr_reader :id, :type, :number_of_legs
 
   def remove_leg
-    remover = Remover.new()
+    remover = Remover.new
     @number_of_legs = remover.decrease(@number_of_legs)
   end
-  
+
   def speak
     'grrrr'
+  end
+
+  def likes_food?(food)
+    @liked_food.liked?(food)
   end
 end
