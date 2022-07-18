830c63c4a4295389ae3a2c1d7992738bae72b7b6
Ronald <rjskinnerc@unal.edu.co>
2022-07-18 16:45:47 -0500

    Add Remover class

--- animal.rb
+++ animal.rb
@@ -1,3 +1,5 @@
+require './remover'
+
 class Animal
   def initialize(type, number_of_legs, name = 'Unknown')
     @id = Random.rand(1..1000)
@@ -9,6 +11,11 @@ class Animal
   attr_accessor :name
   attr_reader :id, :type, :number_of_legs
 
+  def remove_leg
+    remover = Remover.new()
+    @number_of_legs = remover.decrease(@number_of_legs)
+  end
+  
   def speak
     'grrrr'
   end
